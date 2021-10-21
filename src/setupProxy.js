c39e273b202ac3cecdb77b16e66575180da776bd
Derrique B <derriquebaluyut@gmail.com>
2021-10-20 17:45:44 -0700

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    '/api/',
+    '/',
     createProxyMiddleware({
       target: 'https://rocky-castle-64423.herokuapp.com',
       changeOrigin: true,
